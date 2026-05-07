fc94942685cb8c82ba5685f824091a397285f9f0
jaddp19 <slashdoza@gmail.com>
2026-05-07 15:19:04 +0800

    design

--- resources/views/components/navbars/admin/navbar.blade.php
+++ resources/views/components/navbars/admin/navbar.blade.php
@@ -54,9 +54,12 @@
                     <!-- List -->
                     <ul class="flex flex-col gap-y-1">
                         <li>
-                            <a class="w-full flex items-center gap-x-2 py-2 px-2.5 text-medium text-black rounded-lg hover:bg-gray-100 focus:outline-hidden"
+                            <a class="w-full flex items-center gap-x-2 py-2 px-2.5 text-sm text-black rounded-lg hover:bg-gray-100 focus:outline-hidden"
                                 wire:current="bg-gray-100"
                                 href="{{ route('admin.dashboard') }}">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                                </svg>
                                 Dashboard
                             </a>
                         </li>
@@ -66,41 +69,24 @@
 
                 <div
                     class="pt-3 mt-3 flex flex-col border-t border-green-700 first:border-t-0 first:pt-0 first:mt-0">
-                    <span class="block ps-2.5 mb-2 font-extrabold text-xs uppercase text-green-700">
+                    <span class="block ps-2.5 mb-2 font-extrabold text-xs uppercase text-green-700">  
                         Alumni
                     </span>
 
                     <!-- List -->
                     <ul class="flex flex-col gap-y-1">
                         <li>
-                            <a class="w-full flex items-center gap-x-2 py-2 px-2.5 text-medium text-black rounded-lg hover:bg-gray-100 focus:outline-hidden"
+                            <a class="w-full flex items-center gap-x-2 py-2 px-2.5 text-sm text-black rounded-lg hover:bg-gray-100 focus:outline-hidden"
                                 wire:current="bg-gray-100"
                                 href="{{ route('admin.alumni.view') }}">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
+                                </svg>
                                 View All Alumni
                             </a>
                         </li>
                     </ul>
                     <!-- End List -->
-                </div>
-           
-                <div
-                    class="pt-3 mt-3 flex flex-col border-t border-green-700 first:border-t-0 first:pt-0 first:mt-0">
-                    <span class="block ps-2.5 mb-2 font-extrabold text-xs uppercase text-green-700">
-                        Requests
-                    </span>
-
-                    <!-- List -->
-                    <ul class="flex flex-col gap-y-1">
-                        <li>
-                            <a class="w-full flex items-center gap-x-2 py-2 px-2.5 text-medium text-black rounded-lg hover:bg-gray-100 focus:outline-hidden"
-                                wire:current="bg-gray-100"
-                                href="#">
-                                Application Requests
-                            </a>
-                        </li>
-                    </ul>
-                    <!-- End List -->
-
                 </div>
                 
             </nav>
