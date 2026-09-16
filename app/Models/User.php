4185f0d88e3aecb7dddbdf70399421acf075a1a7
jaddp19 <slashdoza@gmail.com>
2026-09-16 21:41:55 +0800

    bugs fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,8 +56,4 @@ class User extends Authenticatable
         return $this->hasOne(Department::class, 'program_head_id');
     }
 
-    public function tracerStudy(): HasOne
-{
-    return $this->hasOne(TracerStudy::class, 'user_id', 'id');
-}
 }
