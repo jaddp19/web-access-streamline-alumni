b90444fe206d5681a77ae3e56cf3d4d9f53e338a
jaddp19 <slashdoza@gmail.com>
2026-09-17 11:07:51 +0800

    bugs fix

--- resources/views/alumni/pages/profile/⚡view-profile/view-profile.blade.php
+++ resources/views/alumni/pages/profile/⚡view-profile/view-profile.blade.php
@@ -18,7 +18,7 @@
             <div class="px-4 pb-4 -mt-16 sm:-mt-20 relative">
                 <div class="flex flex-col md:flex-row md:items-end md:justify-between gap-4">
                     <div class="flex flex-col sm:flex-row sm:items-end gap-4">
-                        <<span
+                        <span
                             class="w-32 h-32 sm:w-40 sm:h-40 rounded-full ring-4 ring-white dark:ring-[#242526] shadow-lg bg-yellow-500 flex items-center justify-center shrink-0">
                             <span class="text-[#0f2b1c] font-bold text-5xl sm:text-6xl"
                                 style="font-family: 'Fraunces', serif;">
